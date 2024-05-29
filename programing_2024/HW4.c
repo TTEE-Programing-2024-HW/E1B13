e4bdf807fe4d788299a16d87986defe3ed8d4e4a
E1B13 <u11202213@o365.ttu.edu.tw>
2024-05-30 02:08:36 +0800

    1

--- programing_2024/HW4.c
+++ programing_2024/HW4.c
@@ -1,11 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <conio.h>
+#include <string.h>
+
+#define MAX_STUDENTS 10
+#define MIN_STUDENTS 5
+
 void clearScreen();//清除螢幕
 void showScream();//展示螢幕風格
+void clearInputBuffer();//清除多餘字符
+
+struct student {
+    char name[50];
+    int id;
+    int math;
+    int physics;
+    int english;
+} 
 
 void cleaScreen(){
     system("CLS||clear");
+}
+
+void clearInputBuffer() {
+    while (getchar() != '\n');
 }
 
 void showScream(){
@@ -37,7 +55,7 @@ void showScream(){
 int main()
 {
     showScream();
-    int error=0,pw;
+    int error=0,pw,n;
     printf("輸入密碼");
     while (error < 3) {
         printf(":");
@@ -58,7 +76,59 @@ int main()
                 switch (R) {
                     case 'a':
                         clearScreen();
-                        printf("輸入一個n(5~10)的整數:");
+                        for (i = 0; i < n; i++) {
+                            printf("請輸入第%d位學生的姓名：\n", i + 1);
+                            scanf("%s", students[i].name);
+
+                            printf("請輸入第%d位學生的學號（6位整數）：\n", i + 1);
+                            while (1) {
+                                    int id;
+                                    if (scanf("%d", &id) == 1 && id >= 100000 && id <= 999999) {
+                                        students[i].id = id;
+                                        break;
+                                    } 
+                                    else {
+                                        printf("輸入錯誤，請輸入一個6位整數：\n");
+                                        clearInputBuffer();
+                                    }
+                                }
+
+                            printf("請輸入第%d位學生的數學成績（0到100）：\n", i + 1);
+                            while (1) {
+                                int math;
+                            if (scanf("%d", &math) == 1 && math >= 0 && math <= 100) {
+                students[i].math = math;
+                break;
+            } else {
+                printf("輸入錯誤，請輸入範圍內的數學成績（0到100）：\n");
+                clearInputBuffer();
+            }
+        }
+
+        printf("請輸入第%d位學生的物理成績（0到100）：\n", i + 1);
+        while (1) {
+            int physics;
+            if (scanf("%d", &physics) == 1 && physics >= 0 && physics <= 100) {
+                students[i].physics = physics;
+                break;
+            } else {
+                printf("輸入錯誤，請輸入範圍內的物理成績（0到100）：\n");
+                clearInputBuffer();
+            }
+        }
+
+        printf("請輸入第%d位學生的英文成績（0到100）：\n", i + 1);
+        while (1) {
+            int english;
+            if (scanf("%d", &english) == 1 && english >= 0 && english <= 100) {
+                students[i].english = english;
+                break;
+            } else {
+                printf("輸入錯誤，請輸入範圍內的英文成績（0到100）：\n");
+                clearInputBuffer();
+            }
+        }
+    }
                         printf("按任意键返回主菜單...\n");
                         getchar();
                         getchar();
